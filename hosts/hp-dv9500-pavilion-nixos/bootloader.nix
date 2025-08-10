f8db417c02412f059b72c6bff136a767d514aaa1
Joseph <emeraldocean123@users.noreply.github.com>
2025-08-09 19:54:34 -0700

    hp-dv9500: add bootloader module and import in host config

--- hosts/hp-dv9500-pavilion-nixos/bootloader.nix
+++ hosts/hp-dv9500-pavilion-nixos/bootloader.nix
@@ -1,3 +1,11 @@
+## hosts/hp-dv9500-pavilion-nixos/bootloader.nix
+# BIOS GRUB bootloader for HP dv9500 (legacy BIOS).
+{ config, lib, pkgs, ... }:
+{
+  # Enable GRUB for legacy BIOS systems. Adjust device if the root disk is different.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "/dev/sda" ];
+}
 { config, ... }:
 {
   # Legacy BIOS default; adjust to EFI on this host if applicable.
