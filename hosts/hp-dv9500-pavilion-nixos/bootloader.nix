5cf9e831d71859293db83b64a7e91b7f8b6e2202
Joseph <emeraldocean123@users.noreply.github.com>
2025-08-09 19:58:21 -0700

    hp-dv9500: fix bootloader.nix duplicate module expression

--- hosts/hp-dv9500-pavilion-nixos/bootloader.nix
+++ hosts/hp-dv9500-pavilion-nixos/bootloader.nix
@@ -6,9 +6,3 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.devices = [ "/dev/sda" ];
 }
-{ config, ... }:
-{
-  # Legacy BIOS default; adjust to EFI on this host if applicable.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "/dev/sda" ];
-}
